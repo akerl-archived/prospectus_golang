c64b033170a686bfcee969e60df9c5149ae7109c
Les Aker <me@lesaker.org>
2019-01-18 20:04:13 -0500

    update to use modules instead of dep

--- lib/prospectus_golang.rb
+++ lib/prospectus_golang.rb
@@ -8,18 +8,19 @@ module ProspectusGolang
     def extended(other) # rubocop:disable Metrics/MethodLength
       dep_list = parse_deps
       other.deps do
-        dep_list.each do |dep_name, current, latest|
+        dep_list.each do |dep_name, tag, hash|
           item do
             name 'dep::' + dep_name
 
             expected do
-              static
-              set latest
+              hash ? github_hash : github_tag
+              repo dep_name
+              filter(/^v\d+\.\d+\.\d+$/) unless hash
             end
 
             actual do
               static
-              set current
+              set(hash || tag)
             end
           end
         end
@@ -29,19 +30,28 @@ module ProspectusGolang
     private
 
     def parse_deps
-      call_dep.map { |x| [x['ProjectRoot'], *parse_version(x)] }
+      list_deps.map do |x|
+        name, version = x.split('@')
+        tag, _, hash = version.split('-')
+        [clean(name), tag, hash ? hash[0..6] : nil]
+      end
     end
 
-    def call_dep
-      o, e, s = Open3.capture3('STATUS_FLAGS=-json make -s status')
+    def list_deps
+      o, e, s = Open3.capture3('go mod graph')
       raise("Command failed: #{e}") unless s.success?
-      JSON.parse(o)
+      name = o.split.first
+      o.split("\n").grep(/^#{name}/).map(&:split).map(&:last)
     end
 
-    def parse_version(object)
-      latest = object['Latest']
-      return [object['Version'], latest] unless object['Version'] =~ /^branch /
-      [object['Revision'][0..8], latest[0..8]]
+    def clean(name)
+      case name
+      when %r{^github\.com/}
+        return name.split('/', 2).last
+      when %r{^golang\.org/x/}
+        return "golang/#{name.split('/').last}"
+      end
+      raise "Name not parsed properly: #{name}"
     end
   end
 end
