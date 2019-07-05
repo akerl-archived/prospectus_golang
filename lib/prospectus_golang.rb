9bb44703675590c5fe75fc4bab991f09bae7632a
Les Aker <me@lesaker.org>
2019-07-05 22:01:07 +0000

    support different endpoint

--- lib/prospectus_golang.rb
+++ lib/prospectus_golang.rb
@@ -5,16 +5,21 @@ module ProspectusGolang
   ##
   # Helper for automatically adding dependency status check
   class Deps < Module
-    def extended(other) # rubocop:disable Metrics/MethodLength
+    def initialize(params = {})
+      @options = params
+    end
+
+    def extended(other) # rubocop:disable Metrics/MethodLength,Metrics/AbcSize
       dep_list = parse_deps
       other.deps do
-        dep_list.each do |dep_name, tag, hash|
+        dep_list.each do |dep_name, api, tag, hash|
           item do
             name 'dep::' + dep_name
 
             expected do
               hash ? github_hash : github_tag
               repo dep_name
+              endpoint(api) if api
               filter(/^v\d+\.\d+\.\d+$/) unless hash
             end
 
@@ -34,7 +39,8 @@ module ProspectusGolang
         name, version = x.split('@')
         tag, _, hash = version.split('-')
         tag.sub!('+incompatible', '')
-        [clean(name), tag, hash ? hash[0..6] : nil]
+        dep_name, api = clean(name)
+        [dep_name, api, tag, hash ? hash[0..6] : nil]
       end
     end
 
@@ -45,18 +51,28 @@ module ProspectusGolang
       o.split("\n").grep(/^#{name}/).map(&:split).map(&:last)
     end
 
-    def clean(name) # rubocop:disable Metrics/MethodLength
+    def clean(name)
       name = name.sub(%r{/v\d+$}, '')
       case name
-      when %r{^github\.com/}
-        return name.split('/', 2).last
       when %r{^golang\.org/x/}
-        return "golang/#{name.split('/').last}"
+        return "golang/#{name.split('/').last}", nil
       when /^gopkg\.in/
         path = name.match(%r{^gopkg\.in/(.*?)(\.v\d+)?$})[1]
-        return path.include?('/') ? path : "go-#{path}/#{path}"
+        return path.include?('/') ? path : "go-#{path}/#{path}", nil
       end
-      raise "Name not parsed properly: #{name}"
+      github_clean(name) || raise("Name not parsed properly: #{name}")
+    end
+
+    def github_map
+      @github_map ||= { 'github.com': nil }.merge(@options[:github_map] || {})
+    end
+
+    def github_clean(name)
+      github_map.each do |web, api|
+        next unless name.start_with? web
+        return name.split('/')[-2..-1].join('/'), api
+      end
+      nil
     end
   end
 end
